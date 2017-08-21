7f8b174955541bf4fdfca1b8ec5ee83137199008
jianbo <iamxjb@gmail.com>
2017-08-21 09:51:27 +0800

    添加阅读数功能

--- wp-rest-api-for-app/wp-rest-api-for-app.php
+++ wp-rest-api-for-app/wp-rest-api-for-app.php
@@ -25,11 +25,16 @@ function custom_fields_rest_prepare_post( $data, $post, $request) {
     $_data['content_first_image']=$images['content_first_image'];
     $_data['post_medium_image_300']=$images['post_medium_image_300'];
     $_data['post_thumbnail_image_624']=$images['post_thumbnail_image_624'];
-    $comments_count = wp_count_comments( $post_id);
+    $comments_count = wp_count_comments($post_id);
+    
+    $pageviews = (int) get_post_meta( $post->ID, 'wl_pageviews',true);
+    $_data[pageviews] = $pageviews;
+    
     $_data['total_comments']=$comments_count->total_comments;
     $category =get_the_category($post_id);
     $_data['category_name'] =$category[0]->cat_name; 
 	$data->data = $_data; 
+    
 	return $data; 
 }
 
@@ -142,7 +147,7 @@ $response = new WP_REST_Response( $data );
 // Add a custom status code
 $response->set_status( 201 ); 
 // Add a custom header
-$response->header( 'Location', 'https://www.watch-life.net' );
+//$response->header( 'Location', 'https://www.watch-life.net' );
 return $response;
 }
 add_action( 'rest_api_init', function () {
@@ -207,7 +212,7 @@ $response = new WP_REST_Response($data);
 // Add a custom status code
 $response->set_status( 201 ); 
 // Add a custom header
-$response->header( 'Location', 'https://www.watch-life.net' );
+//$response->header( 'Location', 'https://www.watch-life.net' );
 return $response;
 }
 
@@ -243,6 +248,62 @@ function get_mostcommented_json($limit = 10) {
 return $posts;    
     
 }
+
+add_action( 'rest_api_init', function () {
+  register_rest_route( 'watch-life-net/v1', 'post/addpageview/(?P<id>\d+)', array(
+    'methods' => 'GET',
+    'callback' => 'updatepageviews'
+  ) );
+} );
+
+
+function updatepageviews($data) {
+    $post_ID =$data['id'];
+    if(!is_numeric($post_ID))
+    {
+        return new WP_Error( 'error', 'ID is not numeric', array( 'status' => 500 ) );
+    }
+    
+    else
+    {
+        $data=post_pageviews_json($post_ID); 
+        if (empty($data)) {
+            return new WP_Error( 'error', 'no find post', array( 'status' => 404 ) );
+          }  
+        // Create the response object
+         $response = new WP_REST_Response($data); 
+        // Add a custom status code
+         $response->set_status( 201 ); 
+        // Add a custom header
+        //$response->header( 'Location', 'https://www.watch-life.net' );
+        return $response;
+    
+    }
+    
+    
+}
+
+function post_pageviews_json($post_ID) {
+          $posts = get_post($post_ID);         
+          if (empty( $posts ) ) {
+            return null;
+            }
+          else
+          {
+             
+              $post_views = (int)get_post_meta($post_ID, 'wl_pageviews', true);  
+              if(!update_post_meta($post_ID, 'wl_pageviews', ($post_views+1)))   
+              {  
+                add_post_meta($post_ID, 'wl_pageviews', 1, true);  
+              } 
+              $result =array();
+              $result["code"]="success";
+              $result["message"]= "update pageviews success  ";
+              $result["status"]="201";
+              return $result;
+          }
+}
+
 
 function getPostImages($post_content){
      $content_first_image= get_post_content_first_image($post_content);           
@@ -309,6 +370,10 @@ function getPostImages($post_content){
             return  $_data;             
            ////////////////////
 }
+
+
+
+
 
 
 
