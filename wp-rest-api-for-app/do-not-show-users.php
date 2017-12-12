1769736901a9a7d8017d4168fcad0b88d04e4013
jianbo <iamxjb@gmail.com>
2017-12-12 14:41:03 +0800

    调整功能

---
+++ wp-rest-api-for-app/do-not-show-users.php
@@ -0,0 +1,11 @@
+<?php
+//禁止在rest api里显示用户列表
+ add_filter( 'rest_endpoints', function( $endpoints ){
+    if ( isset( $endpoints['/wp/v2/users'] ) ) {
+        unset( $endpoints['/wp/v2/users'] );
+    }
+    if ( isset( $endpoints['/wp/v2/users/(?P<id>[\d]+)'] ) ) {
+        unset( $endpoints['/wp/v2/users/(?P<id>[\d]+)'] );
+    }
+    return $endpoints;
+});
