3cad624fc5582cf91bbc1dd4786dc56809a3564b
jianbo <iamxjb@gmail.com>
2018-03-27 08:42:46 +0800

    调整生成小程序二维码的代码

--- wp-rest-api-for-app/weixin-qrcode.php
+++ wp-rest-api-for-app/weixin-qrcode.php
@@ -230,8 +230,154 @@ function get_weixin_qcrode_json($postid,$path,$postImageUrl,$title){
 	  
 	}
 
-  function  creatPoster()
-  {
+ 
+//获取二维码图片
+add_action( 'rest_api_init', function () {
+  register_rest_route( 'watch-life-net/v1', 'weixin/qrcodeimg', array(
+    'methods' => 'POST',
+    'callback' => 'getWinxinQrcodeImg'
+  ) );
+} );
 
+
+function getWinxinQrcodeImg($request) {
+      $postid= $request['postid'];      
+      $path=$request['path'];
+      $openid =$request['openid']; 
+
+    if(empty($openid) || empty($postid)  || empty($path))
+        {
+            return new WP_Error( 'error', ' openid   or postid or path   empty', array( 'status' => 500 ) );
+        }
+        else if(get_post($postid)==null)
+        {
+             return new WP_Error( 'error', 'post id is error ', array( 'status' => 500 ) );
+        }
+        else
+        {
+              if(!username_exists($openid))
+            {
+                return new WP_Error( 'error', 'Not allowed to submit', array('status' => 500 ));
+            }
+            else if(is_wp_error(get_post($postid)))
+            {
+                 return new WP_Error( 'error', 'post id is error ', array( 'status' => 500 ) );
+            }
+            else
+            {
+
+              $data=get_weixin_qcrodeimg_json($postid,$path); 
+              if (empty($data)) {
+                  return new WP_Error( 'error', 'creat qrcode error', array( 'status' => 404 ) );
+                }
+               $response = new WP_REST_Response($data);
+               $response->set_status( 200 ); 
+               return $response;
+
+            }
+        }
+
+      
+
+}
+
+function get_weixin_qcrodeimg_json($postid,$path){      
+           
+      $qrcodeName = 'qrcode-'.$postid.'.png';//文章小程序二维码文件名     
+      $qrcodeurl = WP_REST_API_FOR_APP_PLUGIN_DIR.'qrcode/'.$qrcodeName;//文章小程序二维码路径
+      
+      
+      //自定义参数区域，可自行设置      
+      $appid = get_option('wf_appid');
+      $appsecret = get_option('wf_secret');
+     
+      //判断文章小程序二维码是否存在，如不存在，在此生成并保存
+      if(!is_file($qrcodeurl)) {
+          //$ACCESS_TOKEN = getAccessToken($appid,$appsecret,$access_token);
+          $access_token_url = 'https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid='.$appid.'&secret='.$appsecret;
+           $access_token_result = https_request($access_token_url);
+           if($access_token_result !="ERROR")
+            {
+              $access_token_array= json_decode($access_token_result,true);
+              if(empty($access_token_array['errcode']))
+              {
+                $access_token =$access_token_array['access_token'];
+                if(!empty($access_token))
+                {
+
+                  //接口A小程序码,总数10万个（永久有效，扫码进入path对应的动态页面）
+                  $url = 'https://api.weixin.qq.com/wxa/getwxacode?access_token='.$access_token;
+                  //接口B小程序码,不限制数量（永久有效，将统一打开首页，可根据scene跟踪推广人员或场景）
+                  //$url = "https://api.weixin.qq.com/wxa/getwxacodeunlimit?access_token=".$ACCESS_TOKEN;
+                  //接口C小程序二维码,总数10万个（永久有效，扫码进入path对应的动态页面）
+                  //$url = 'http://api.weixin.qq.com/cgi-bin/wxaapp/createwxaqrcode?access_token='.$ACCESS_TOKEN;
+
+                  //header('content-type:image/png');
+                  $color = array(
+                      "r" => "0",  //这个颜色码自己到Photoshop里设
+                      "g" => "0",  //这个颜色码自己到Photoshop里设
+                      "b" => "0",  //这个颜色码自己到Photoshop里设
+                  );
+                  $data = array(
+                      //$data['scene'] = "scene";//自定义信息，可以填写诸如识别用户身份的字段，注意用中文时的情况
+                      //$data['page'] = "pages/index/index";//扫码后对应的path，只能是固定页面
+                      'path' => $path, //前端传过来的页面path
+                      'width' => intval(100), //设置二维码尺寸
+                      'auto_color' => false,
+                      'line_color' => $color,
+                  );
+                  $data = json_encode($data);
+                  //可在此处添加或者减少来自前端的字段
+                  $QRCode = get_content_post($url,$data);//小程序二维码
+                  if(empty($QRCode['errcode']))
+                  {
+                    //输出二维码
+                    file_put_contents($qrcodeurl,$QRCode);
+                    //imagedestroy($QRCode);
+                    $flag=true;
+                  }
+                  
+                }
+                else
+                {
+                  $flag=false;
+                }
+
+              }
+              else
+              {
+                $flag=false;
+              }
+
+            }
+            else
+            {
+              $flag=false;
+            }
+          
+      }
+      else
+      {
+
+        $flag=true;
+      }
+
+      if($flag)
+      {
+        $result["code"]="success";
+          $result["message"]= "qrcode  creat  success"; 
+          $result["status"]="200"; 
+          return $result;
+
+      }
+      else {
+          $result["code"]="success";
+          $result["message"]= "qrcode  creat  error"; 
+          $result["status"]="500"; 
+          return $result;
+      } 
+
+
+      
     
   }
