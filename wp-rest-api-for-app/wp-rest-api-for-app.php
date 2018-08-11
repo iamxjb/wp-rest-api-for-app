8216b0d08c3e5687c3410a3a86b8a4d29ed1ad82
jianbo <iamxjb@gmail.com>
2018-08-11 12:58:27 +0800

    修复评论无法显示的问题

--- wp-rest-api-for-app/wp-rest-api-for-app.php
+++ wp-rest-api-for-app/wp-rest-api-for-app.php
@@ -3,7 +3,7 @@
 Plugin Name: WP REST API For App
 Plugin URI: http://www.watch-life.net
 Description: 为微信小程序、app提供定制WordPress rest api
-Version: 2.0
+Version: 2.1
 Author: jianbo
 Author URI: http://www.watch-life.net
 License: GPL v3
