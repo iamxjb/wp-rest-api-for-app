853940c49e568f7de2d680d5f7b2a03d7770444c
jianbo <iamxjb@gmail.com>
2018-09-22 17:02:21 +0800

    完善文章中的post_all_images字段

--- wp-rest-api-for-app/api.php
+++ wp-rest-api-for-app/api.php
@@ -100,7 +100,8 @@ function getPostImages($content,$postId){
         $post_full_image=$content_first_image;
      }
 
-     $post_all_images = get_attached_media( 'image', $postId);
+     //$post_all_images = get_attached_media( 'image', $postId);
+     $post_all_images= get_post_content_images($content);
 
      $_data['post_frist_image']=$post_frist_image;
      $_data['post_thumbnail_image']=$post_thumbnail_image;
@@ -118,98 +119,40 @@ function getPostImages($content,$postId){
 
 
     return  $_data; 
-    
-
-   //  if($thumbnail_id ){
-   //      $thumb = wp_get_attachment_image_src($thumbnail_id, 'thumbnail');
-   //      $post_thumbnail_image = $thumb[0];
-   //  }
-   //  else if($content_first_image)
-   //  {          
-   //      $attachments = get_attached_media( 'image', $post_id ); //查找文章的附件
-   //      $index = array_keys($attachments);
-   //      $flag=0; 
-        
-   //      for ($i = 0; $i < sizeof($index); $i++) {
-   //          $arr =$attachments[$index[$i]];
-   //          $imageName = $arr->{"post_title"};            
-   //          if(strpos($content_first_image,$imageName)!==false){  //附件的名称如果和第一张图片相同,就取这个附件的缩略图
-   //              {
-   //                  $post_thumbnail_image_150 = wp_get_attachment_image_url($arr->{"ID"},'thumbnail');
-   //                  $post_medium_image_300=wp_get_attachment_image_url($arr->{"ID"},'medium');
-   //                  $post_thumbnail_image_624=wp_get_attachment_image_url($arr->{"ID"},'post-thumbnail');
-   //                  $id =$arr->{"ID"};                    
-   //                  $flag++;
-   //                  break;
-   //              }
-   //          }
-   //      }
-   //      if($flag>0)
-   //      {
-   //          $post_thumbnail_image = $post_thumbnail_image_150;
-   //      }
-   //      else
-   //      {
-   //          $post_thumbnail_image = $content_first_image; 
-   //      }          
-   //  }
-   //  else
-   //  {
-   //      $post_thumbnail_image='';
-   //  }   
-
-   //  if(strlen($post_medium_image_300)>0)
-   //  {
-   //      $_data['post_medium_image_300']=$post_medium_image_300; 
-   //  }
-   //  else
-   //  {
-   //     $_data['post_medium_image_300']=$content_first_image;
-   // }  
-   // if(strlen($post_thumbnail_image_624)>0)
-   // {
-   //  $_data['post_thumbnail_image_624']=$post_thumbnail_image_624; 
-   //  }
-   //  else
-   //  {
-   //     $_data['post_thumbnail_image_624']=$content_first_image;
-   //  }
-   //  
-               
+           
 
 }
 
-// function GetIP()
-// {
-//     foreach (array('HTTP_CLIENT_IP', 'HTTP_X_FORWARDED_FOR', 'HTTP_X_FORWARDED', 'HTTP_X_CLUSTER_CLIENT_IP', 'HTTP_FORWARDED_FOR', 'HTTP_FORWARDED', 'REMOTE_ADDR') as $key)
-//     {
-//         if (array_key_exists($key, $_SERVER) === true)
-//         {
-//             foreach (array_map('trim', explode(',', $_SERVER[$key])) as $ip)
-//             {
-//                 if (filter_var($ip, FILTER_VALIDATE_IP, FILTER_FLAG_NO_PRIV_RANGE | FILTER_FLAG_NO_RES_RANGE) !== false)
-//                 {
-//                     return $ip;
-//                 }
-//             }
-//         }
-//     }
-// }
+function get_post_content_images($post_content){
+    if(!$post_content){
+        $the_post       = get_post();
+        $post_content   = $the_post->post_content;
+    } 
+
+    
+
+    preg_match_all('|<img.*?src=[\'"](.*?)[\'"].*?>|i', do_shortcode($post_content), $matches);
+    $images=array();
+    if($matches && isset($matches[1]))
+    {
+        $_images=$matches[1]; 
+       
+        for($i=0; $i<count($matches[1]);$i++) {
+            $imageurl['imagesurl'] =$matches[1][$i];
+            $imageurl['id'] ='image'.$i;
+            $images[]=$imageurl;
+            
+        }
+        
+        return $images;
+
+    }
+
+    return null;
+        
+}
 
 
-// function httpGet($url) {
-//     $curl = curl_init();
-//     curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
-//     curl_setopt($curl, CURLOPT_TIMEOUT, 500);
-//     curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
-//     curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, false);
-//     curl_setopt($curl, CURLOPT_URL, $url);
-
-//     $res = curl_exec($curl);
-//     curl_close($curl);
-
-//     return $res;
-// }
 //等比例缩小图片，处理二维码
 function PicCompress($src,$out_with=100){
     // 获取图片基本信息
